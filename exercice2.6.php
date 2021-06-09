e69afc03b972d123916df00c4ecd0604d950def8
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 15:17:43 +0200

    correction ajout HTML

--- exercice2.6.php
+++ exercice2.6.php
@@ -1,11 +1,25 @@
-<?php
-$age = "19";
-if ($age >= "18")
-{
-    echo "Tu es majeur";
-}
-else
-{
-    echo "Tu n'es pas majeur";
-}
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 2.6</title>
+</head>
+<body>
+    <h1>Exercice 6 Partie 2 : Les conditions PHP</h1>
+    <p>
+        <?php
+        $age = "19";
+        if ($age >= "18")
+        {
+            echo "Tu es majeur";
+        }
+        else
+        {
+            echo "Tu n'es pas majeur";
+        }
+        ?>
+    </p>
+</body>
+</html>
